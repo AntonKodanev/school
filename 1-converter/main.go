c4304481f56936ae10171e6783b2b1b783453ad1
Anton Kodanev <akodanev@exolve.ru>
2026-02-03 20:16:27 +0300

    1-start-commit2

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
 	const usdToEur = 0.84
 	const usdToRub = 77.02
 	const eurToRub = usdToRub / usdToEur
+}
+
+func userValueCurrency() (float64, string, string) {
+	var quantity float64
+	var convertFrom, convertTo string
+	fmt.Print("Введите количество валюты: ")
+	fmt.Scan(&quantity)
+	fmt.Print("Введите из какой валюты конвертировать (rub, usd, eur): ")
+	fmt.Scan(&convertFrom)
+	fmt.Print("Введите в какую валюту конвертировать (rub, usd, eur): ")
+	fmt.Scan(&convertTo)
+	return quantity, convertFrom, convertTo
+}
+
+func convertMoney(q float64, cf string, ct string) (cm float64) {
+	return 0.0
 }
